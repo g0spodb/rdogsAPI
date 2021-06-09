8a416cb5fa191d2a8b9f3a0523f64ba9b8320bbf
Николаев Клементий <vapenationeron@gmail.com>
2021-06-09 13:18:05 +0300

    Добавьте файлы проекта.

---
+++ rdogsAPI/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace rdogsAPI
+{
+    struct Dogs {
+    public string message;
+    public string status;
+            }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = $"https://dog.ceo/api/breeds/image/random";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                var randomDog = JsonConvert.DeserializeObject<Dogs>(result);
+                Console.WriteLine(randomDog.message);
+            }
+
+        }
+    }
+}
